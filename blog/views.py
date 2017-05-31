85e82fc1d9d83c03b694478aa6ef8d4bb5a3768d
Albert Ventura <albert@venturads.com>
2017-05-31 02:39:27 +0000

    django python with bootstrap time

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+import datetime
+from django import template
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, render
+
+def index(request):
+    return HttpResponse("Hello Django")
+
+def today_is(request):
+    now = datetime.datetime.now()
+    return render(request, 'blog/datetime.html', {'now': now })
